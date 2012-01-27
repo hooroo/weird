85bd1b0436b61ae05951d80017a71293b5b242cb
Daniel Vydra <usefulaccount@gmail.com>
2012-01-27 13:47:32 +1100

    Version spec fix

--- spec/weird_spec.rb
+++ spec/weird_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Weird do
   it 'should return correct version string' do
-    Weird.version_string.should == "Weird version #{Weird::VERSION}"
+    Weird.version_string.should == "Weird version: #{Weird::VERSION}"
   end
 
   describe "#valid_ird?" do
